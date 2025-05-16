aa643363c48b507fb5529c56c3dca9ca86d61261
jennydev <jennydev2001@gmail.com>
2025-05-16 20:33:17 +0545

    routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import authRouter from "./routes/auth.route";
 
 const app = express()
+
+app.use("/api/auth", authRouter)
 
 app.listen(5001, ()=>{
     console.log("Server is running on port 5001");
