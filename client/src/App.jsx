186503116548f48cd1167121267eae5842ace59f
jennydev <jennydev2001@gmail.com>
2025-05-24 13:45:02 +0545

    frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Signup from './pages/Signup'
+import Login from './pages/LOgin'
+import Setting from './pages/Setting'
+import Profile from './pages/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Home/>
+      <Navbar/>
+
+      <Routes>
+        <Route path='/' element= {<Home/>} />
+        <Route path='/signup' element={<Signup/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/setting' element={<Setting/>} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+
     </div>
   )
 }
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>LOgin</div>
+  )
+}
+
+export default Login
